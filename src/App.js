c26a0c4b416308447ffedbbb575bb192c7903087
Alex <74793187+MasterTatto@users.noreply.github.com>
2021-07-07 19:03:40 +0300

    finish lesson 19

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import './App.css';
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 function App() {
     return (
-        <div className={'app-wrapper'}>
-            <Header/>
-            <NavBar/>
-            <Profile/>
-        </div>
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
+                <Header/>
+                <NavBar/>
+
+                <div className={'app-wrapper-content'}>
+                    <Route component={Profile} path={'/profile'}/>
+                    <Route component={Dialogs} path={'/dialogs'}/>
+                    <Route component={News} path={'/news'}/>
+                    <Route component={Music} path={'/music'}/>
+                    <Route component={Settings} path={'/settings'}/>
+                    {/*<Profile/>*/}
+                    {/*<Dialogs/>*/}
+                </div>
+
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import s from './NavBar.module.css' ;
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
     return (
         <nav className={s.nav}>
             <ul>
-
-                <li className={`${s.item} ${s.active}`}><a href={'#'}>Profile</a></li>
-                <li className={`${s.item} ${s.active}`}><a href={'#'}>Messages</a></li>
-                <li className={`${s.item} ${s.active}`}><a href={'#'}>News</a></li>
-                <li className={`${s.item} ${s.active}`}><a href={'#'}>Music</a></li>
-                <li className={`${s.item} ${s.active}`}><a href={'#'}>Settings</a></li>
+                <li className={`${s.item} ${s.active}`}><NavLink activeClassName={s.active} to={'/profile'}>Profile</NavLink></li>
+                <li className={`${s.item} ${s.active}`}><NavLink activeClassName={s.active} to={'/dialogs'}>Messages</NavLink></li>
+                <li className={`${s.item} ${s.active}`}><NavLink activeClassName={s.active} to={'/news'}>News</NavLink></li>
+                <li className={`${s.item} ${s.active}`}><NavLink activeClassName={s.active} to={'/music'}>Music</NavLink></li>
+                <li className={`${s.item} ${s.active}`}><NavLink activeClassName={s.active} to={'/settings'}>Settings</NavLink></li>
             </ul>
         </nav>
     );
